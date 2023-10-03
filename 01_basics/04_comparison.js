ded8d2ba1ab8519cd826ffac188d3fcf592a0549
Shubham Rawat <130495806+shubham-rawat-dev@users.noreply.github.com>
2023-10-03 11:44:09 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+// console.log(2>=1)
+// console.log(2==2);
+// console.log("2"<=2); here string "2" gets converted to number 2
+// console.log("2"==2);
+//console.log(null<=0); here null gets converted to equivalent number which is 0
+//console.log(null==0); false because equality check works differently than comparison checks
+//console.log(undefined<0);any opration withh undefined results false
+console.log("2"===2);
