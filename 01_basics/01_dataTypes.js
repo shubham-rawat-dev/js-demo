afcc7abd7ecd4df11683676584c5c8cdc10b0a9b
Shubham Rawat <130495806+shubham-rawat-dev@users.noreply.github.com>
2023-10-03 10:54:18 +0000

    conversion done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+// alert(23+23)
+/* alert is not used in js environments
+primitive data types
+number
+string
+boolean
+bigint
+undefined
+symbol
+null=standalone value its type=object
+
+
+
+*/
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score=true
+// console.log(typeof score);
+// let intValueOfScore=Number(score);
+// console.log(typeof intValueOfScore);
+// console.log(intValueOfScore);
+//"33"=>33 number
+// "33abc"=>NaN number
+//undefined=>NaN number
+//null=>0 number
+//boolean true=>1 number false=>0 number
+// "shubham" NaN number
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+let someNumber="33"
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
